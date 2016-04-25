d1a07fc1c07ff7b1ef9413d80b0dc053cb2618cf
BeanLiu <njueebeanliu@hotmail.com>
2016-04-26 00:11:47 +0800

    仿照rs里的地图写了个窗口一样的东西 实际上感觉和窗口差的有点远 可以扔个背景过来在下面

---
+++ NJULoginTest/WindowBL.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace NJULoginTest
+{
+    public sealed partial class WindowBL : UserControl,INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string title = "";
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Title)));
+            }
+        }
+
+        
+        public object InnerContent
+        {
+            get { return InnerView.Content; }
+            set { InnerView.Content=value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(InnerContent)));
+            }
+        }
+
+        public WindowBL()
+        {
+            this.InitializeComponent();
+        }
+
+        private Visibility iswindowvisible;
+        public Visibility IsWindowVisible
+        {
+            get
+            {
+                return iswindowvisible;
+            }
+            set
+            {
+                iswindowvisible = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsWindowVisible)));
+                if (iswindowvisible == Visibility.Visible)
+                {
+                    Minimal.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    Minimal.Visibility = Visibility.Visible;
+                }
+            }
+        }
+        
+        private void shrink_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (IsWindowVisible == Visibility.Collapsed)
+            {
+                IsWindowVisible = Visibility.Visible;
+            }
+            else
+            {
+                IsWindowVisible = Visibility.Collapsed;
+            }
+        }
+    }
+}
